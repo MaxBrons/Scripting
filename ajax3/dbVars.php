4cd1c6e1afb838cddf90cf391d832bfabcf36731
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2019-05-06 11:41:40 +0200

    Update dbVars.php

--- ajax3/dbVars.php
+++ ajax3/dbVars.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "127.0.0.1";
 $uid = "c3609Main";
-$pwd = "DIT_IS_MIJN__SUPER_STERKE_wachtwoord";
+$pwd = "<input type='password'>DIT_IS_MIJN__SUPER_STERKE_wachtwoord</input>";
 $database = "c3609Main";
  ?>
